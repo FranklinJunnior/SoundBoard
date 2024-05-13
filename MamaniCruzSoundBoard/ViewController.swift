953ecd97903b750730c37a25ca74fa4ccc449b74
Franklin <franklin.mamani.m@tecsup.edu.pe>
2024-05-13 11:34:07 -0700

    Initial Commit

---
+++ MamaniCruzSoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MamaniCruzSoundBoard
+//
+//  Created by Franklin Junnior on 5/13/24.
+//  Copyright © 2024 franklin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
