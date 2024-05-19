9c9ba15332fa3aa07f43ef01337a580b4689ea51
Franklin <franklin.mamani.m@tecsup.edu.pe>
2024-05-19 00:19:32 -0700

    Laboratorio y Tarea Completado !!!

--- MamaniCruzSoundBoard/SoundViewController.swift
+++ MamaniCruzSoundBoard/SoundViewController.swift
@@ -1,57 +1,68 @@
-//
-//  SoundViewController.swift
-//  MamaniCruzSoundBoard
-//
-//  Created by Franklin Junnior on 5/18/24.
-//  Copyright © 2024 franklin. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 import CoreData
 
-
-
-
-class SoundViewController: UIViewController {
+class SoundViewController: UIViewController, AVAudioRecorderDelegate {
     
     @IBOutlet weak var grabarButton: UIButton!
     @IBOutlet weak var reproducirButton: UIButton!
     @IBOutlet weak var nombreTextField: UITextField!
     @IBOutlet weak var agregarButton: UIButton!
-    var grabarAudio:AVAudioRecorder?
-    var reproducirAudio:AVAudioPlayer?
-    var audioURL:URL?
+    @IBOutlet weak var tiempoGrabacion: UILabel!
+    @IBOutlet weak var volumenSlider: UISlider!
     
+    
+    var grabarAudio: AVAudioRecorder?
+    var reproducirAudio: AVAudioPlayer?
+    var audioURL: URL?
+    var recordingTimer: Timer?
     
     @IBAction func grabarTapped(_ sender: Any) {
-        if grabarAudio!.isRecording {
-            // Detener la grabación
+        if grabarAudio?.isRecording == true {
             grabarAudio?.stop()
-            // Cambiar el texto del botón grabar
             grabarButton.setTitle("GRABAR", for: .normal)
-            // Habilitar el botón de reproducir
             reproducirButton.isEnabled = true
             agregarButton.isEnabled = true
+            stopRecordingTimer()
         } else {
-            // Empezar a grabar
             grabarAudio?.record()
-            // Cambiar el texto del botón grabar a detener
             grabarButton.setTitle("DETENER", for: .normal)
-            // Deshabilitar el botón de reproducir
             reproducirButton.isEnabled = false
+            startRecordingTimer()
         }
     }
-	
-    
     
     @IBAction func reproducirTapped(_ sender: Any) {
         do {
             try reproducirAudio = AVAudioPlayer(contentsOf: audioURL!)
             reproducirAudio!.play()
-            print("Reproduciendo")
-        } catch {}
+            print("REPRODUCIENDO")
+        } catch {
+            print("Error al reproducir el audio: \(error)")
+        }
     }
+    
+     @IBAction func volumenChanged(_ sender: UISlider) {
+         let volumen = sender.value
+         
+         if volumen < 0.1 {
+             mostrarAlerta(mensaje: "El volumen es muy bajo")
+         }
+        
+         if volumen > 0.9 {
+             mostrarAlerta(mensaje: "El volumen es muy alto")
+         }
+         
+         if let reproducirAudio = reproducirAudio {
+             reproducirAudio.volume = volumen
+         }
+     }
+
+     func mostrarAlerta(mensaje: String) {
+         let alerta = UIAlertController(title: "Advertencia", message: mensaje, preferredStyle: .alert)
+         alerta.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+         present(alerta, animated: true, completion: nil)
+     }
     
     
     @IBAction func agregarTapped(_ sender: Any) {
@@ -59,89 +70,84 @@ class SoundViewController: UIViewController {
         let grabacion = Grabacion(context: context)
         grabacion.nombre = nombreTextField.text
         grabacion.audio = NSData(contentsOf: audioURL!)! as Data
+        
+        // Obtener la duración del audio desde el archivo
+        do {
+            let audioFile = try AVAudioFile(forReading: audioURL!)
+            let duration = Double(audioFile.length) / audioFile.fileFormat.sampleRate
+            grabacion.duracion = duration
+        } catch {
+            print("Error al obtener la duración del audio: \(error)")
+            grabacion.duracion = 0.0
+        }
+        
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
-        navigationController!.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
     }
 
     
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         configurarGrabacion()
         reproducirButton.isEnabled = false
         agregarButton.isEnabled = false
-        
-
-        // Do any additional setup after loading the view.
+        volumenSlider.value = 0.5
     }
     
-    func configurarGrabacion(){
-
-    do{
-
-    //creando sesion de audio
-
-    let session = AVAudioSession.sharedInstance()
-
-    try session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options:[])
-
-    try session.overrideOutputAudioPort(.speaker)
-
-    try session.setActive(true)
-
-    //creando direccion para el archivo de audio
-
-    let basePath:String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-
-    let pathComponents = [basePath,"audio.m4a"]
-
-    audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
-
-    //impresion de ruta donde se guardan los archivos
-
-    print("********************")
-
-    print(audioURL!)
-
-    print("********************")
-
-    //crear opciones para el grabador de audio
-
-    var settings:[String:AnyObject] = [:]
-
-    settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
-
-    settings[AVSampleRateKey] = 44100.0 as AnyObject?
-
-    settings[AVNumberOfChannelsKey] = 2 as AnyObject?
-
-    //crear el objeto de grabacion de audio
-
-    grabarAudio = try AVAudioRecorder(url: audioURL!, settings: settings)
-
-    grabarAudio!.prepareToRecord()
-
-    } catch let error as NSError{
-
-    print(error)
-
+    func configurarGrabacion() {
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(.playAndRecord, mode: .default, options: [])
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            let basePath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+            
+            var settings: [String: AnyObject] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC) as AnyObject?
+            settings[AVSampleRateKey] = 44100.0 as AnyObject?
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject?
+            
+            grabarAudio = try AVAudioRecorder(url: audioURL!, settings: settings)
+            grabarAudio?.delegate = self
+            grabarAudio!.prepareToRecord()
+        } catch let error as NSError {
+            print("Error al configurar la grabación: \(error)")
+        }
     }
-
-    }
-
-
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            grabarButton.setTitle("GRABAR", for: .normal)
+            reproducirButton.isEnabled = true
+            agregarButton.isEnabled = true
+            stopRecordingTimer()
+        } else {
+            print("Error en la grabación")
+        }
     }
-    */
-
+    
+    func startRecordingTimer() {
+        recordingTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateRecordingDuration), userInfo: nil, repeats: true)
+    }
+    
+    func stopRecordingTimer() {
+        recordingTimer?.invalidate()
+        recordingTimer = nil
+    }
+    
+    @objc func updateRecordingDuration() {
+        if let currentTime = grabarAudio?.currentTime {
+            tiempoGrabacion.text = formatTime(currentTime)
+        }
+    }
+    
+    func formatTime(_ time: TimeInterval) -> String {
+        let minutes = Int(time) / 60
+        let seconds = Int(time) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
 }
+
--- MamaniCruzSoundBoard/ViewController.swift
+++ MamaniCruzSoundBoard/ViewController.swift
@@ -1,26 +1,30 @@
-//
-//  ViewController.swift
-//  MamaniCruzSoundBoard
-//
-//  Created by Franklin Junnior on 5/13/24.
-//  Copyright © 2024 franklin. All rights reserved.
-//
-
 import UIKit
 import CoreData
 import AVFoundation
-
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tablaGrabaciones: UITableView!
     var grabaciones: [Grabacion] = []
-    var reproducirAudio:AVAudioPlayer?
+    var reproducirAudio: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tablaGrabaciones.dataSource = self
         tablaGrabaciones.delegate = self
+        tablaGrabaciones.register(UITableViewCell.self, forCellReuseIdentifier: "audioCell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            grabaciones = try context.fetch(Grabacion.fetchRequest())
+            tablaGrabaciones.reloadData()
+        } catch {
+            print("Error al cargar grabaciones: \(error)")
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,55 +32,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "audioCell", for: indexPath)
         let grabacion = grabaciones[indexPath.row]
-        cell.textLabel?.text = grabacion.nombre
-        return cell
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         
-        do {
-            grabaciones = try context.fetch(Grabacion.fetchRequest())
-            tablaGrabaciones.reloadData()
-        } catch {}
+        let duracionText = String(format: "%.2f segundos", grabacion.duracion)
+        cell.textLabel?.text = "\(grabacion.nombre ?? "Sin Nombre") - \(duracionText)"
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let grabacion = grabaciones[indexPath.row]
         
         do {
-            reproducirAudio = try AVAudioPlayer(data:
-                grabacion.audio! as Data)
+            reproducirAudio = try AVAudioPlayer(data: grabacion.audio! as Data)
             reproducirAudio?.play()
-        } catch { }
+        } catch {
+            print("Error al reproducir grabación: \(error)")
+        }
         
-        tablaGrabaciones.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             let grabacion = grabaciones[indexPath.row]
-            let context = (UIApplication.shared.delegate as!
-    AppDelegate).persistentContainer.viewContext
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
             context.delete(grabacion)
-            (UIApplication.shared.delegate as!
-            AppDelegate).saveContext()
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
             
             do {
-                grabaciones = try
-    context.fetch(Grabacion.fetchRequest())
-                tablaGrabaciones.reloadData()
-            } catch {  }
+                grabaciones = try context.fetch(Grabacion.fetchRequest())
+                tableView.reloadData()
+            } catch {
+                print("Error al eliminar grabación: \(error)")
+            }
         }
     }
-
-
-
-
 }
-
 
